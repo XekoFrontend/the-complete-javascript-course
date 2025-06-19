8559c0e9b2f74439c5f3bda6e14227823fa65846
XekoFrontend <haocenter@gmail.com>
2025-06-19 18:26:55 +0700

    EDIT: learning how to named a variable in JavaScript. Renamed 'JS_Fundamentals_1' folder

---
+++ JS_Fundamentals_1/Link a JavaScript file/script.js
@@ -0,0 +1,29 @@
+// link a JavaScript
+let js= 'amazing';
+// if (js === 'amazing') alert('JavaScript is fun');
+
+let simpleMath = 50+6-7/8*9;
+console.log(simpleMath);
+
+
+// Quy tắc đặt tên Variables
+    /*
+    1. camelcase
+    2. mổ tả rõ ràng
+    3. không dùng số ở đầu, có thể dùng _ hoặc $
+    4. chỉ chứa chữ cái, số hoặc 2 ký tự _ và $
+    5. tránh dùng các từ khóa trùng với JavaScript, ví dụ: new, function ...
+    */
+let myFirstName = 'Haruka';
+    // Trường hợp đặc biệt
+let PI = 3.1415;
+
+
+// assignments: Values and Variables
+let myCountry = "Cambodia";
+let myContient = "Asia";
+let ourPopulation = 90;
+
+console.log(myCountry);
+console.log(myContient);
+console.log(ourPopulation);
