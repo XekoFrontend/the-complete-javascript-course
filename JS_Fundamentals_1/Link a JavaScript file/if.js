7140d31b5268be404c925ca9174a85a19dd9e919
XekoFrontend <haocenter@gmail.com>
2025-06-22 18:00:00 +0700

    LEARNING: IF statement. example 2.

--- JS_Fundamentals_1/Link a JavaScript file/if.js
+++ JS_Fundamentals_1/Link a JavaScript file/if.js
@@ -3,9 +3,20 @@
  * IF
  * ------------------------------------------------------------------------
  */
+// example 1
 const age = 16;
 if(age >= 18){
     console.log('You can start driving license.');
 } else {
     console.log(`You have to wait for ${18 - age} years.`);
 }
+
+// Example 2
+const year = 2993;
+let century;
+if (year > 2000){
+    century = 21;
+} else {
+    century = 20;
+}
+console.log(century);
