99f9f9647abda31a6fa17ca2d017acfe29a37863
XekoFrontend <haocenter@gmail.com>
2025-06-23 09:51:19 +0700

    ADDED: Assignment.js

---
+++ JS_Fundamentals_1/basic/assignments.js
@@ -0,0 +1,35 @@
+console.log('******* ASSIGNMENTS *******');
+/**
+ * ------------------------------------------------------------------------
+ * LECTURE: Values and Variables
+ * ------------------------------------------------------------------------
+  */
+
+let country = "Cambodia";
+let continent = "Asia";
+let population = 90;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+/**
+ * ------------------------------------------------------------------------
+ * LECTURE: Data Types
+ * ------------------------------------------------------------------------
+ */
+
+const isIsland = true;
+let language;
+console.log(
+    typeof(language),
+    typeof (population),
+    typeof(country),
+    typeof (isIsland),
+);
+
+/**
+ * ------------------------------------------------------------------------
+ * LECTURE: let, const and var
+ * ------------------------------------------------------------------------
+ */
