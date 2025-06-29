6b987138149dabeb1aff97ddaa8aef7dce5d3016
XekoFrontend <haocenter@gmail.com>
2025-06-29 10:38:16 +0700

    EDIT: move if examples from 'if.js' to 'script.js' and then delete 'if.js'

--- JS_Fundamentals_1/basic/script.js
+++ JS_Fundamentals_1/basic/script.js
@@ -131,6 +131,29 @@ console.log('57' * '2' / 6 - '4');   // 15
 
 /**
  * ------------------------------------------------------------------------
+ * IF
+ * ------------------------------------------------------------------------
+ */
+// example 1
+const age = 16;
+if(age >= 18){
+    console.log('You can start driving license.');
+} else {
+    console.log(`You have to wait for ${18 - age} years.`);
+}
+
+// Example 2
+const year = 2993;
+let century;
+if (year > 2000){
+    century = 21;
+} else {
+    century = 20;
+}
+console.log(century);
+
+/**
+ * ------------------------------------------------------------------------
  * Truthy and Falsy Values
  * ------------------------------------------------------------------------
  *  Trong boolean operator thì 5 giá trị dưới sẽ cho kết quả false.
